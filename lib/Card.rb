76e79d7a226e24af0e97a94b2fbbefa8df22d4ef
Oliver Antcliff <antcliffoc@gmail.com>
2019-08-27 14:44:33 +0100

    Add touch_in, touch_out, deduct

--- lib/Card.rb
+++ lib/Card.rb
@@ -1,12 +1,32 @@
 class Card
   attr_reader :balance
+  attr_reader :touched_in
   $DEFAULT_LIMIT = 90
+
   def initialize(limit = $DEFAULT_LIMIT)
     @balance = 0
     @limit = limit
+    @touched_in = false
   end
+
   def top_up(value)
     raise "exceeds £#{$DEFAULT_LIMIT}" if @balance + value > @limit
     @balance += value
-    end
+  end
+
+  def deduct(value)
+    @balance -= value
+  end
+
+  def touch_in
+    @touched_in = true
+  end
+
+  def touch_out
+    @touched_in = false
+  end
+
+  def in_journey?
+    @touched_in
+  end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -12,4 +12,48 @@ describe Card do
         expect{ subject.top_up(91) }.to raise_error("exceeds £#{$DEFAULT_LIMIT}")
       end
     end
+
+    describe ".deduct" do
+      it "deducts value from balance" do
+        subject.top_up(10)
+        subject.deduct(6)
+        expect(subject.balance).to eq(4)
+      end
+    end
+
+    describe ".touch_in" do
+      it "sets card as touched_in" do
+        subject.touch_in
+        expect(subject.touched_in).to eq(true)
+      end
+    end
+
+    describe ".touch_out" do
+      it "sets card as touched_in" do
+        subject.touch_in
+        subject.touch_out
+        expect(subject.touched_in).to eq(false)
+      end
+    end
+
+    describe ".in_journey?" do
+      context "when in journey" do
+        before do
+          subject.touch_in
+        end
+        it "returns true" do
+          expect(subject).to be_in_journey
+        end
+      end
+      context "when not in journey" do
+        before do
+          subject.touch_out
+        end
+        it "returns false" do
+          expect(subject).not_to be_in_journey
+        end
+      end
+    end
+
+
   end
